2b6b322e81edec6ad34b67410ab082af108d6a82
CHRISKINUNGI <chrskinungi@gmail.com>
2023-08-04 12:32:12 +0300

    commit to add  4-repetition_token_3.rb  file

---
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+# Define the regular expression pattern
+regex = /hbt*n/
+
+# Extract the first command-line argument
+input_text = ARGV[0]
+
+# Use the regular expression to find matches in the input text
+matches = input_text.scan(regex)
+
+# Join the matches into a single string, if any
+matched_text = matches.join
+
+# Print the matched text
+puts matched_text
